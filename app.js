4338a6e4fa7763d197684a190f994fa1421ee0c1
Rupesh Jha <127649834+viperven@users.noreply.github.com>
2023-12-09 16:44:02 +0530

    my code app.js

--- app.js
+++ app.js
@@ -1,76 +1,56 @@
-alert("hello");
-const addButton = document.getElementById("add");
+const btnAdd = document.getElementById("add");
 
-//ADDIND DATA IN LOCAL STORAGE;
-const updateLsData = () => {
-  const textAreaData = document.querySelectorAll("#txtarea");
-  const notes = [];
-  // console.log(textAreaData);
-  textAreaData.forEach((note) => {
-    return notes.push(note.value);
+addToLS = () => {
+  let notesArray = [];
+  let notes = document.querySelectorAll("#txtarea");
+
+  notes.forEach((curElm) => {
+    notesArray.push(curElm.value);
   });
-  localStorage.setItem("notes", JSON.stringify(notes));
+  localStorage.setItem("notes", JSON.stringify(notesArray));
 };
+addNewNote = (text) => {
+  let notes = document.createElement("div");
+  notes.classList.add("note-area");
 
-const addNewNote = (text = "") => {
-  //.createElement is used to create element.
-  const note = document.createElement("div");
-  //.classlist to add multiple class names
-  note.classList.add("note-area");
-  //paste entire str
-  const htmlData = `
-    <textarea name="" id="txtarea"></textarea> 
-     <div class="operation flx gap-5">
+  let htmlData = `
+  <textarea name="" id="txtarea"></textarea>
+  <div class="operation flx gap-5">
       <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
       <button class="delete"><i class="fa-solid fa-trash"></i></button>
-     </div>
- `;
+  </div> `;
 
-  //add directly all html part with this inside note {AFTER-BEGIN}
-  note.insertAdjacentHTML("afterbegin", htmlData);
-  //finally add to document body appendChild: append to last child of node
-  //used children array bcz i want to put inside grid-3c and position is 2
-  document.body.children[2].appendChild(note);
+  notes.insertAdjacentHTML("afterbegin", htmlData);
+  document.body.children[2].appendChild(notes);
 
-  //getting References
-  //notice here we have mention note. bcz now everthing is inside note not document
-  const textArea = note.querySelector("#txtarea");
-  const operationDiv = note.querySelector(".operation");
-  const editButton = note.querySelector(".edit");
-  const deleteButton = note.querySelector(".delete");
+  const btnEdit = notes.querySelector(".edit");
+  const btnDelete = notes.querySelector(".delete");
+  const textArea = notes.querySelector("#txtarea");
 
-  //deleting Node
+  text ? (textArea.value = text) : (textArea.value = "");
 
-  deleteButton.addEventListener("click", () => {
-    note.remove();
-    //inbuilt func to remove node
+  textArea.addEventListener("change", (event) => {
+    boxValue = event.target.value;
+    textArea.toggleAttribute("disabled");
+    addToLS();
   });
 
-  //IMP LINE
-  textArea.value = text;
-
-  //toggle between edit button
-  editButton.addEventListener("click", () => {
-    //on clikc add diabled attribute clik again remove
+  btnEdit.addEventListener("click", () => {
     textArea.toggleAttribute("disabled");
   });
 
-  textArea.addEventListener("change", (event) => {
-    //change is lot better than input
-    //this is gives us value when we click outside or some change happens
-    const value = event.target.value;
-    textArea.value = value;
-    updateLsData();
+  btnDelete.addEventListener("click", () => {
+    console.log(notes);
+    notes.remove();
+    addToLS();
   });
 };
 
-//GETTING DATA BACK FROM LOCAL STORAGE;
-const notes = JSON.parse(localStorage.getItem("notes"));
-if (notes) {
-  notes.forEach((note) => {
-    addNewNote(note);
-  });
-}
+let boxValue = JSON.parse(localStorage.getItem("notes"));
+boxValue.forEach((curElm) => {
+  if (curElm.length != 0) {
+    addNewNote(curElm);
+  }
+});
 
-//on button click
-addButton.addEventListener("click", () => addNewNote());
+btnAdd.addEventListener("click", () => addNewNote());
