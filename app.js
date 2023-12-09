d90e90059bd222f628e083d635089ef43cae741a
Rupesh Jha <127649834+viperven@users.noreply.github.com>
2023-12-09 22:11:32 +0530

    app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ addToLS = () => {
   localStorage.setItem("notes", JSON.stringify(notesArray));
 };
 addNewNote = (text) => {
-  alert("hello");
   let notes = document.createElement("div");
   notes.classList.add("note-area");
 
@@ -55,5 +54,4 @@ boxValue.forEach((curElm) => {
 });
 
 btnAdd.addEventListener("click", () => addNewNote());
-btnAdd.addEventListener("touchstart", () => addNewNote();
 //final
